fb4a86bd0a476c3b5463db8cf1164b2bb7036863
Dat Nguyen <datmieu2004@gmail.com>
2025-05-06 11:39:44 +0700

    Added component dashboard and changed routes

--- frontend/departmnet-management/src/routes/routeConfig.js
+++ frontend/departmnet-management/src/routes/routeConfig.js
@@ -6,6 +6,12 @@ import TaskDetail from '../Components/TaskDetail/TaskDetail';
 import OtherTask from '../Components/OtherTask/OtherTask'; 
 import React from 'react'; // Import React để tạo các trang trống
 
+
+import HouseholdDashboard from '../Components/dashboard/householdDashboard/HouseholdDashboard';
+import EmployeeDashboard from '../Components/dashboard/employeeDashboard/EmployeeDashboard';
+import ServiceDashboard from '../Components/dashboard/serviceDashboard/ServiceDashboard';
+
+
 // Các trang trống ban đầu
 const AccountPage = () => <div><h1>Trang Tài khoản</h1></div>;
 const OtherTask = () => <div><h1>Trang Nhiệm vụ người khác</h1></div>;
@@ -16,7 +22,16 @@ export const routes = [
   { path: "/admin/account", layout: AdminLayout, component: AccountPage },
   { path: "/admin/other-tasks", layout: AdminLayout, component: OtherTask },
   { path: "/admin/other-accounts", layout: AdminLayout, component: AccountPage },
-  { path: "/admin/dashboard", layout: AdminLayout, component: DashboardPage },
+  { 
+    path: "/admin/dashboard", 
+    layout: AdminLayout, 
+    component: DashboardPage,
+    children: [
+      { path: "households", layout: AdminLayout, component: HouseholdDashboard },
+      { path: "employees", layout: AdminLayout, component: EmployeeDashboard },
+      { path: "services", layout: AdminLayout, component: ServiceDashboard }
+    ]
+  },
   { path: "/leader/account", layout: LeaderLayout, component: AccountPage },
   { path: "/leader/tasks", layout: LeaderLayout, component: TaskList },
   { path: "/emp/account", layout: EmpLayout, component: AccountPage },
